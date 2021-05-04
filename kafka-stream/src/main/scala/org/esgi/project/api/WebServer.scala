3b4df696a9429fb137cd7ac6c6cc69f1efcd034e
Dreamsplutox <predalienregel@gmail.com>
2021-05-04 19:13:54 +0200

    *

---
+++ kafka-stream/src/main/scala/org/esgi/project/api/WebServer.scala
@@ -0,0 +1,166 @@
+package org.esgi.project.api
+
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
+import org.apache.kafka.streams.{KafkaStreams, StoreQueryParameters}
+import org.apache.kafka.streams.state.{QueryableStoreTypes, ReadOnlyKeyValueStore, ReadOnlyWindowStore, WindowStoreIterator}
+import org.esgi.project.api.models.{MeanLatencyForURLResponse, VisitCountResponse}
+import org.esgi.project.streaming.StreamProcessing
+import org.esgi.project.streaming.models.MeanLatencyForURL
+
+import java.time.Instant
+import scala.jdk.CollectionConverters._
+
+/**
+ * -------------------
+ * Part.3 of exercise: Interactive Queries
+ * -------------------
+ */
+object WebServer extends PlayJsonSupport {
+  def routes(streams: KafkaStreams): Route = {
+    concat(
+      path("visits" / Segment) { period: String =>
+        get {
+          period match {
+            case "30s" =>
+              // load our materialized store
+              val kvStore30Seconds: ReadOnlyWindowStore[String, Long] = streams
+                .store(
+                  StoreQueryParameters
+                    .fromNameAndType(StreamProcessing.thirtySecondsStoreName, QueryableStoreTypes.windowStore[String, Long]())
+                )
+              // fetch all available keys
+              val availableKeys = kvStore30Seconds.all().asScala.map(_.key.key()).toList.distinct
+              // define our time interval to fetch the last window (nearest one to now)
+              val toTime = Instant.now()
+              val fromTime = toTime.minusSeconds(30)
+
+              complete(
+                availableKeys
+                  .map(storeKeyToVisitCount(kvStore30Seconds, fromTime, toTime))
+                  .sortBy(_.count)(implicitly[Ordering[Long]].reverse)
+              )
+            case "1m" =>
+              // load our materialized store
+              val kvStore1Minute: ReadOnlyWindowStore[String, Long] = streams
+                .store(StoreQueryParameters.fromNameAndType(StreamProcessing.lastMinuteStoreName, QueryableStoreTypes.windowStore[String, Long]()))
+              // fetch all available keys
+              val availableKeys = kvStore1Minute.all().asScala.map(_.key.key()).toList.distinct
+              // define our time interval to fetch the last window (nearest one to now)
+              val toTime = Instant.now()
+              val fromTime = toTime.minusSeconds(60)
+
+              complete(
+                availableKeys
+                  .map(storeKeyToVisitCount(kvStore1Minute, fromTime, toTime))
+                  .sortBy(_.count)(implicitly[Ordering[Long]].reverse)
+              )
+            case "5m" =>
+              // load our materialized store
+              val kvStore5Minute: ReadOnlyWindowStore[String, Long] = streams
+                .store(StoreQueryParameters.fromNameAndType(StreamProcessing.lastFiveMinutesStoreName, QueryableStoreTypes.windowStore[String, Long]()))
+              // fetch all available keys
+              val availableKeys = kvStore5Minute.all().asScala.map(_.key.key()).toList.distinct
+              // define our time interval to fetch the last window (nearest one to now)
+              val toTime = Instant.now()
+              val fromTime = toTime.minusSeconds(5 * 60)
+
+              complete(
+                availableKeys
+                  .map(storeKeyToVisitCount(kvStore5Minute, fromTime, toTime))
+                  .sortBy(_.count)(implicitly[Ordering[Long]].reverse)
+              )
+            case _ =>
+              // unhandled period asked
+              complete(
+                HttpResponse(StatusCodes.NotFound, entity = "Not found")
+              )
+          }
+        }
+      },
+      path("visits-per-category" / Segment) { period: String =>
+        get {
+          period match {
+            case "30s" =>
+              // load our materialized store
+              val kvStore30Seconds: ReadOnlyWindowStore[String, Long] = streams
+                .store(StoreQueryParameters.fromNameAndType(StreamProcessing.thirtySecondsByCategoryStoreName, QueryableStoreTypes.windowStore[String, Long]()))
+              // fetch all available keys
+              val availableKeys = kvStore30Seconds.all().asScala.map(_.key.key()).toList.distinct
+              // define our time interval to fetch the last window (nearest one to now)
+              val toTime = Instant.now()
+              val fromTime = toTime.minusSeconds(30)
+
+              complete(
+                availableKeys
+                  .map(storeKeyToVisitCount(kvStore30Seconds, fromTime, toTime))
+                  .sortBy(_.count)(implicitly[Ordering[Long]].reverse)
+              )
+            case "1m" =>
+              // load our materialized store
+              val kvStore1Minute: ReadOnlyWindowStore[String, Long] = streams
+                .store(StoreQueryParameters.fromNameAndType(StreamProcessing.lastMinuteByCategoryStoreName, QueryableStoreTypes.windowStore[String, Long]()))
+              // fetch all available keys
+              val availableKeys = kvStore1Minute.all().asScala.map(_.key.key()).toList.distinct
+              // define our time interval to fetch the last window (nearest one to now)
+              val toTime = Instant.now()
+              val fromTime = toTime.minusSeconds(60)
+
+              complete(
+                availableKeys
+                  .map(storeKeyToVisitCount(kvStore1Minute, fromTime, toTime))
+                  .sortBy(_.count)(implicitly[Ordering[Long]].reverse)
+              )
+            case "5m" =>
+              // load our materialized store
+              val kvStore5Minute: ReadOnlyWindowStore[String, Long] = streams
+                .store(StoreQueryParameters.fromNameAndType(StreamProcessing.lastFiveMinutesByCategoryStoreName, QueryableStoreTypes.windowStore[String, Long]()))
+              // fetch all available keys
+              val availableKeys = kvStore5Minute.all().asScala.map(_.key.key()).toList.distinct
+              // define our time interval to fetch the last window (nearest one to now)
+              val toTime = Instant.now()
+              val fromTime = toTime.minusSeconds(5 * 60)
+
+              complete(
+                availableKeys
+                  .map(storeKeyToVisitCount(kvStore5Minute, fromTime, toTime))
+                  .sortBy(_.count)(implicitly[Ordering[Long]].reverse)
+              )
+            case _ =>
+              // unhandled period asked
+              complete(
+                HttpResponse(StatusCodes.NotFound, entity = "Not found")
+              )
+          }
+        }
+      },
+      path("latency" / "beginning") {
+        get {
+          // load our materialized store
+          val kvStoreMeanLatencyPerURL: ReadOnlyKeyValueStore[String, MeanLatencyForURL] = streams
+            .store(StoreQueryParameters.fromNameAndType(StreamProcessing.meanLatencyForURLStoreName, QueryableStoreTypes.keyValueStore[String, MeanLatencyForURL]()))
+          // fetch all available keys
+          val availableKeys: List[String] = kvStoreMeanLatencyPerURL.all().asScala.map(_.key).toList
+
+          complete(
+            availableKeys
+              .map(storeKeyToMeanLatencyForURL(kvStoreMeanLatencyPerURL))
+              .sortBy(_.meanLatency)(implicitly[Ordering[Long]].reverse)
+          )
+        }
+      }
+    )
+  }
+
+  def storeKeyToVisitCount(store: ReadOnlyWindowStore[String, Long], from: Instant, to: Instant)(key: String): VisitCountResponse = {
+    val row: WindowStoreIterator[Long] = store.fetch(key, from, to)
+    VisitCountResponse(url = key, count = row.asScala.toList.last.value)
+  }
+
+  def storeKeyToMeanLatencyForURL(store: ReadOnlyKeyValueStore[String, MeanLatencyForURL])(key: String): MeanLatencyForURLResponse = {
+    val row: MeanLatencyForURL = store.get(key)
+    MeanLatencyForURLResponse(url = key, meanLatency = row.meanLatency)
+  }
+}
---
+++ kafka-stream/src/main/scala/org/esgi/project/streaming/StreamProcessing.scala
@@ -0,0 +1,124 @@
+package org.esgi.project.streaming
+
+import io.github.azhur.kafkaserdeplayjson.PlayJsonSupport
+import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.kstream.{JoinWindows, TimeWindows, Windowed}
+import org.apache.kafka.streams.scala._
+import org.apache.kafka.streams.scala.kstream._
+import org.esgi.project.streaming.models.{MeanLatencyForURL, Metric, Visit, VisitWithLatency}
+
+import java.io.InputStream
+import java.time.Duration
+import java.util.Properties
+
+object StreamProcessing extends PlayJsonSupport {
+
+  import org.apache.kafka.streams.scala.ImplicitConversions._
+  import org.apache.kafka.streams.scala.serialization.Serdes._
+
+  val applicationName = "web-events-stream-app-teacher-2"
+  val visitsTopicName: String = "visits"
+  val metricsTopicName: String = "metrics"
+
+  val thirtySecondsStoreName: String = "VisitsOfLast30Seconds"
+  val lastMinuteStoreName = "VisitsOfLastMinute"
+  val lastFiveMinutesStoreName = "VisitsOfLast5Minutes"
+
+  val thirtySecondsByCategoryStoreName: String = "VisitsOfLast30SecondsByCategory"
+  val lastMinuteByCategoryStoreName = "VisitsOfLastMinuteByCategory"
+  val lastFiveMinutesByCategoryStoreName = "VisitsOfLast5MinutesByCategory"
+  val meanLatencyForURLStoreName = "MeanLatencyForURL"
+
+  val props: Properties = buildProperties
+
+  // defining processing graph
+  val builder: StreamsBuilder = new StreamsBuilder
+
+  // topic sources
+  val visits: KStream[String, Visit] = builder.stream[String, Visit](visitsTopicName)
+  val metrics: KStream[String, Metric] = builder.stream[String, Metric](metricsTopicName)
+
+  /**
+   * -------------------
+   * Part.1 of exercise
+   * -------------------
+   */
+  // Repartitioning visits with visit URL as key, then group them by key
+  val visitsGroupedByUrl: KGroupedStream[String, Visit] = visits
+    .map((_, visit) => (visit.url, visit))
+    .groupByKey
+
+  val visitsOfLast30Seconds: KTable[Windowed[String], Long] = visitsGroupedByUrl
+    .windowedBy(TimeWindows.of(Duration.ofSeconds(30)).advanceBy(Duration.ofSeconds(1)))
+    .count()(Materialized.as(thirtySecondsStoreName))
+
+  val visitsOfLast1Minute: KTable[Windowed[String], Long] = visitsGroupedByUrl
+    .windowedBy(TimeWindows.of(Duration.ofMinutes(1)).advanceBy(Duration.ofMinutes(1)))
+    .count()(Materialized.as(lastMinuteStoreName))
+
+  val visitsOfLast5Minute: KTable[Windowed[String], Long] = visitsGroupedByUrl
+    .windowedBy(TimeWindows.of(Duration.ofMinutes(5)).advanceBy(Duration.ofMinutes(1)))
+    .count()(Materialized.as(lastFiveMinutesStoreName))
+
+  /**
+   * -------------------
+   * Part.2 of exercise
+   * -------------------
+   */
+  val visitsGroupedByCategory: KGroupedStream[String, Visit] = visits
+    .map((_, visit) => (visit.url.split("/")(1), visit))
+    .groupByKey(Grouped.`with`)
+
+  val visitsOfLast30SecondsByCategory: KTable[Windowed[String], Long] = visitsGroupedByCategory
+    .windowedBy(TimeWindows.of(Duration.ofSeconds(30)).advanceBy(Duration.ofSeconds(1)))
+    .count()(Materialized.as(thirtySecondsByCategoryStoreName))
+
+  val visitsOfLast1MinuteByCategory: KTable[Windowed[String], Long] = visitsGroupedByCategory
+    .windowedBy(TimeWindows.of(Duration.ofMinutes(1)).advanceBy(Duration.ofMinutes(1)))
+    .count()(Materialized.as(lastMinuteByCategoryStoreName))
+
+  val visitsOfLast5MinuteByCategory: KTable[Windowed[String], Long] = visitsGroupedByCategory
+    .windowedBy(TimeWindows.of(Duration.ofMinutes(5)).advanceBy(Duration.ofMinutes(1)))
+    .count()(Materialized.as(lastFiveMinutesByCategoryStoreName))
+
+  val visitsWithMetrics: KStream[String, VisitWithLatency] = visits
+    .join(metrics)({ (visit, metric) =>
+      VisitWithLatency(_id = visit._id, timestamp = visit.timestamp, sourceIp = visit.sourceIp, url = visit.url, latency = metric.latency)
+    }, JoinWindows.of(Duration.ofSeconds(5)))
+
+  val meanLatencyPerUrl: KTable[String, MeanLatencyForURL] = visitsWithMetrics
+    .map((_, visitWithLatency) => (visitWithLatency.url, visitWithLatency))
+    .groupByKey
+    .aggregate(MeanLatencyForURL.empty) { (_, newVisitWithLatency, accumulator) =>
+      accumulator.increment(latency = newVisitWithLatency.latency).computeMeanLatency
+    }(Materialized.as(meanLatencyForURLStoreName))
+
+  def run(): KafkaStreams = {
+    val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
+    streams.start()
+
+    // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
+    Runtime.getRuntime.addShutdownHook(new Thread(new Runnable() {
+      override def run() {
+        streams.close()
+      }
+    }))
+    streams
+  }
+
+  // auto loader from properties file in project
+  def buildProperties: Properties = {
+    import org.apache.kafka.clients.consumer.ConsumerConfig
+    import org.apache.kafka.streams.StreamsConfig
+    val inputStream: InputStream = getClass.getClassLoader.getResourceAsStream("kafka.properties")
+
+    val properties = new Properties()
+    properties.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationName)
+    // Disable caching to print the aggregation value after each record
+    properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0")
+    properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+    properties.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, "-1")
+    properties.load(inputStream)
+    properties
+  }
+}
